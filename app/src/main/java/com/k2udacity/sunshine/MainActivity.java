8cb503cc84745a38b661b9127a8dc434ad64d577
Kinshuk Chandra <kinshuk.ram@gmail.com>
2016-01-24 19:54:10 +0530

    Showing sorting only in the main activity

--- app/src/main/java/com/k2udacity/sunshine/MainActivity.java
+++ app/src/main/java/com/k2udacity/sunshine/MainActivity.java
@@ -221,6 +221,8 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_sort, menu);
+
         MenuItem item = menu.findItem(R.id.action_sort);
 
         View view = MenuItemCompat.getActionView(item);
